5ede10713caea795569f11f9648691ddf7034c6b
NikitaSabne <nikitas@incubxperts.com>
2022-07-21 17:23:20 +0530

    dev-Nikita created hashtable

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+namespace HashTable {
+    public class Program {
+        static void Main(string[] args) {
+            Hashtable hashdata = new Hashtable();
+            hashdata.Add(101,"harry");
+            hashdata.Add(102, "greko");
+            hashdata.Add(103, "malphoy");
+            hashdata.Add(104, "harmieny");
+            hashdata.Add(105, "ginger");
+            Console.WriteLine("keys   Values");
+            Console.WriteLine("--------------");
+            foreach (DictionaryEntry k in hashdata) {                
+                Console.WriteLine(k.Key+"   "+k.Value);
+            }
+            Console.WriteLine("\n");
+            hashdata[103] = "dumbledore";
+            Console.WriteLine("updated 103th index hashtable: ");
+            foreach (DictionaryEntry k in hashdata) {
+                Console.WriteLine(k.Key + "   " + k.Value);
+            }
+        }
+    }
+}
