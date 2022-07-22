fe86b646d687d7eadf78b71dbf1822a15e36def3
NikitaSabne <nikitas@incubxperts.com>
2022-07-22 11:23:33 +0530

    dev-Nikita created stack

--- Program.cs
+++ Program.cs
@@ -3,61 +3,10 @@ using System.Collections;
 namespace HashTable {
     public class Program {
         static void Main(string[] args) {
-            Hashtable hashdata = new Hashtable();
-            Hashtable hashdata3 = new Hashtable();
-
-            hashdata.Add(101, "harry");
-            hashdata.Add(102, "greko");
-            hashdata.Add(103, "malphoy");
-            hashdata.Add(104, "harmieny");
-            hashdata.Add(105, "ginger");
-
-            Console.WriteLine("keys   Values");
-            Console.WriteLine("--------------");
-            foreach (DictionaryEntry k in hashdata) {
-                Console.WriteLine(k.Key + "   " + k.Value);
-            }
-
-            Console.WriteLine("\n");
-            hashdata[103] = "dumbledore";
-            Console.WriteLine("updated 103th index value in hashtable: ");
-            foreach (DictionaryEntry k in hashdata) {
-                Console.WriteLine(k.Key + "   " + k.Value);
-            }
-
-            Console.WriteLine("\n");
-            hashdata.Remove(103);
-            Console.WriteLine("Removed 103th index value: ");
-            foreach (DictionaryEntry k in hashdata) {
-                Console.WriteLine(k.Key + "   " + k.Value);
-            }
-
-            Console.WriteLine("\n checking existing key and value");
-            bool decision = hashdata.Contains(101);
-            Console.WriteLine(decision);
-            Console.WriteLine(hashdata.ContainsKey(101));
-            Console.WriteLine(hashdata.ContainsValue("greko"));
-
-            Console.WriteLine("\n");
-            DictionaryEntry[] hashdata2 = new DictionaryEntry[hashdata.Count];
-            hashdata.CopyTo(hashdata2, 0);                       
-            Console.WriteLine("copying hashdata values to hashdata2");
-            foreach (DictionaryEntry k in hashdata2) {
-                Console.WriteLine(k.Key + "   " + k.Value);
-            }
-
-            hashdata3 = hashdata;
-            Console.WriteLine("copying hashdata values to hashdata2 by another way: ");
-            foreach (DictionaryEntry k in hashdata3) {
-                Console.WriteLine(k.Key + "   " + k.Value);
-            }
-
-            Console.WriteLine("\n");
-            hashdata.Clear();
-            Console.WriteLine("cleared table: ");
-            foreach (DictionaryEntry k in hashdata) {
-                Console.WriteLine(k.Key + "   " + k.Value);
-            }
+            //HashTableMethods ht = new HashTableMethods();
+            //ht.HashMethods();
+            StackMethods st = new StackMethods();
+            st.StackFun();
         }
     }
 }
---
+++ StackMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashTable {
+    public class StackMethods {
+        public void StackFun() {
+            Stack s = new Stack();
+
+            s.Push("Foo");
+            s.Push(102);
+            s.Push("Bar");
+            s.Push(104);
+            s.Push("Zoo");
+
+            Console.WriteLine("\nIterating Stack: ");
+            foreach (var i in s) {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\nremoving element using pop method ");
+            Console.WriteLine(s.Pop());     //Removes and returns the object at the top
+
+            Console.WriteLine("\nIterating Stack after removing top element: ");
+            foreach (var i in s) {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\nReturning top element using peek method ");
+            Console.WriteLine(s.Peek());    //Returns the object at the top
+
+            Console.WriteLine("\nCheck existance of Foo element");
+            Console.WriteLine(s.Contains("Foo"));       //case sensitive
+            Console.WriteLine("\nCheck existance of zoo element");
+            Console.WriteLine(s.Contains("Zoo"));
+
+            Console.WriteLine("\nIterating Stack-2 after cloning stack: ");
+            foreach (var i in s) {
+                Console.WriteLine(i);
+            }
+
+            s.Clear();
+            Console.WriteLine("\nIterating Stack after cleaning stack: ");
+            foreach (var i in s) {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
